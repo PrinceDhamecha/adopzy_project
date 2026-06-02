7422884b8cf6f58b68c65e3ee02de7289dd608c2
PrinceDhamecha <princedhamecha03@gmail.com>
2026-06-02 15:59:35 +0530

    2nd commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,21 @@
 const mysql = require('mysql2');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
+
+let ssl = undefined;
+if (process.env.DB_SSL === 'true') {
+  if (process.env.DB_CA) {
+    ssl = { ca: process.env.DB_CA, rejectUnauthorized: true };
+  } else {
+    const caPath = path.join(__dirname, 'ca.pem');
+    if (fs.existsSync(caPath)) {
+      ssl = { ca: fs.readFileSync(caPath), rejectUnauthorized: true };
+    } else {
+      ssl = { rejectUnauthorized: false };
+    }
+  }
+}
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
@@ -10,9 +26,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
   queueLimit: 0,
-  ssl: process.env.DB_SSL === 'true' ? {
-    rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false'
-  } : undefined
+  ssl
 });
 
 module.exports = pool.promise();
